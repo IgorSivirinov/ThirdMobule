1bb582eb6bfc16e18311dc13a9a477355f0dcb40
IgorSivirinov <igorek7937@gmail.com>
2019-12-29 20:13:18 +0200

    Patch 2

--- app/src/main/java/com/example/thirdmobule/InputColorActivity.java
+++ app/src/main/java/com/example/thirdmobule/InputColorActivity.java
@@ -6,21 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 public class InputColorActivity extends AppCompatActivity {
-    private Boolean ic;
     private Button ds;
+    private CheckBox up, down;
+    private RadioButton bred, bgreen, bblue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_color);
-        ds = (Button)findViewById(R.id.ds);
-        ds.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent DoSa = new Intent(InputColorActivity.this,DowSavActivity.class);
-                startActivity(DoSa);
-            }
-        });
+
+        up = (CheckBox)findViewById(R.id.up);
+        down = (CheckBox)findViewById(R.id.down);
+        bred = (RadioButton) findViewById(R.id.bred);
+        bgreen = (RadioButton) findViewById(R.id.bgrean);
+        bblue = (RadioButton) findViewById(R.id.bblue);
+
+            ds = (Button) findViewById(R.id.ds);
+            ds.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    boolean dr = bred.isChecked();
+                    boolean dg = bgreen.isChecked();
+                    boolean db = bblue.isChecked();
+                    boolean du = up.isChecked();
+                    boolean dd = down.isChecked();
+
+                    Intent  icm = new Intent (InputColorActivity.this,MainActivity.class);
+
+                    icm.putExtra("dr",dr);
+                    icm.putExtra("dg",dg);
+                    icm.putExtra("db",db);
+                    icm.putExtra("du",du);
+                    icm.putExtra("dd",dd);
+
+                    startActivity(icm);
+                }
+            });
+
     }
+
 }
--- app/src/main/java/com/example/thirdmobule/MainActivity.java
+++ app/src/main/java/com/example/thirdmobule/MainActivity.java
@@ -6,14 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private Button ic;
+    private Button ic, bch, bcl;
+    private LinearLayout ll1, ll2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ic=(Button)findViewById(R.id.ic);
+
+        ll1 = (LinearLayout)findViewById(R.id. ll1);
+        ll2 = (LinearLayout)findViewById(R.id. ll2);
+        ic  = (Button)findViewById(R.id.ic);
+        bch = (Button)findViewById(R.id.bch);
+        bcl = (Button)findViewById(R.id.bcl);
+
         ic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,5 +31,40 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(InCo);
             }
         });
+
+        bch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean dr = getIntent().getExtras().getBoolean("dr");
+                boolean dg = getIntent().getExtras().getBoolean("dg");
+                boolean db = getIntent().getExtras().getBoolean("db");
+                boolean du = getIntent().getExtras().getBoolean("du");
+                boolean dd = getIntent().getExtras().getBoolean("dd");
+
+                if (dr) {
+                    if (du) ll1.setBackgroundColor(getColor(R.color.cored));
+                    if (dd) ll2.setBackgroundColor(getColor(R.color.cored));
+                }
+                if (dg) {
+                    if (du) ll1.setBackgroundColor(getColor(R.color.cogreen));
+                    if (dd) ll2.setBackgroundColor(getColor(R.color.cogreen));
+                }
+                if (db) {
+
+                    if (du) ll1.setBackgroundColor(getColor(R.color.coblue));
+                    if (dd) ll2.setBackgroundColor(getColor(R.color.coblue));
+                }
             }
+        });
+
+        bcl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ll1.setBackgroundColor(getColor(R.color.conorm));
+                ll2.setBackgroundColor(getColor(R.color.conorm));
+            }
+        });
+
+    }
 }
